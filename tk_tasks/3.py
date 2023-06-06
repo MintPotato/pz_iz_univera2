d8e234a0172b876d59fcec000656779b24d3e78d
ginger dog <104458002+MintPotato@users.noreply.github.com>
2023-06-06 16:41:38 +0300

    Update 3.py

--- tk_tasks/3.py
+++ tk_tasks/3.py
@@ -2,9 +2,18 @@ import tkinter as tk
 import tkinter.ttk as ttk
 from math import pi
 from tkinter import Canvas
+from abc import ABC, abstractmethod
 
 
-class Circle:
+class Figure(ABC):
+    def __init__(self):
+        ...
+
+    def find_surface(self):
+        ...
+
+
+class Circle(Figure):
     def __init__(self, cm=1):
         self.r = cm * 38  # 38 - kоличество пикселей в одном сантиметре как утверждает какой-то сайт
 
@@ -12,7 +21,7 @@ class Circle:
         return pi * (self.r // 38) ** 2
 
 
-class Square:
+class Square(Figure):
     def __init__(self, cm=1):
         self.side = cm * 38
 
@@ -20,7 +29,7 @@ class Square:
         return (self.side // 38) ** 2
 
 
-class Polygon:
+class Polygon(Figure):
     def __init__(self, a_cm=1, b_cm=2):
         self.a_side = a_cm * 38
         self.b_side = b_cm * 38
@@ -52,35 +61,45 @@ class MainWindow(tk.Tk):
 class DrawWindow(tk.Toplevel):
     def __init__(self, figure):
         super().__init__()
-        self.geometry('200x200')
+        self.figure = figure
+        self.label_vvod = ttk.Label(self)
+        self.label_vvod.pack()
+        self.ent = ttk.Entry(self)
+        self.ent.pack()
+        self.btn = ttk.Button(self, text='Подтвердить', command=self.draw).pack()
         self.lbl = ttk.Label(self, text='')
-        self.lbl.pack()  # .place(x=0, y=0, height=20)
-        self.canvas = Canvas(self)
+        self.geometry('200x200')
 
-        if figure == 'c':
-            self.draw_circle()
-        elif figure == 's':
-            self.draw_square()
+        if self.figure == 'c':
+            self.label_vvod.config(text='Введите радиус круга')
+        elif self.figure == 's':
+            self.label_vvod.config(text='Введите сторону квадрата')
         else:
-            self.draw_polygon()
+            self.label_vvod.config(text='Введите стороны прямоугольника через пробел')
+            self.geometry('300x200')
 
-    def draw_circle(self):
-        circle = Circle()
-        self.canvas.create_oval(2, 10, circle.r * 2, 10 + circle.r * 2, fill='#1f1')
-        self.canvas.pack()
-        self.lbl.config(text=f'Площадь фигуры равна: {circle.find_surface()}')
-
-    def draw_square(self):
-        square = Square()
-        self.canvas.create_rectangle(2, 10, square.side, 10 + square.side, fill='#1f1')
-        self.lbl.config(text=f'Площадь фигуры равна: {square.find_surface()}')
+        self.lbl.pack()
+        self.canvas = Canvas(self, width=2000, height=2000)
         self.canvas.pack()
 
-    def draw_polygon(self):
-        polygon = Polygon()
-        self.canvas.create_rectangle(2, 10, polygon.a_side, 10 + polygon.b_side, fill='#1f1')
-        self.lbl.config(text=f'Площадь фигуры равна: {polygon.find_surface()}')
-        self.canvas.pack()
+    def draw(self):
+        if self.figure == 'c':
+            circle = Circle(cm=int(self.ent.get()))
+            self.canvas.create_oval(2, 10, circle.r * 2, 10 + circle.r * 2, fill='#1f1')
+            self.canvas.pack()
+            self.lbl.config(text=f'Площадь фигуры равна: {circle.find_surface()}')
+        elif self.figure == 's':
+            square = Square(cm=int(self.ent.get()))
+            self.canvas.create_rectangle(2, 10, square.side, 10 + square.side, fill='#1f1')
+            self.lbl.config(text=f'Площадь фигуры равна: {square.find_surface()}')
+            self.canvas.pack()
+        else:
+            if len(self.ent.get().split()) == 2:
+                sides = list(map(int, self.ent.get().split()))
+                polygon = Polygon(sides[0], sides[1])
+                self.canvas.create_rectangle(2, 10, polygon.a_side, 10 + polygon.b_side, fill='#1f1')
+                self.lbl.config(text=f'Площадь фигуры равна: {polygon.find_surface()}')
+                self.canvas.pack()
 
 
 if __name__ == '__main__':
